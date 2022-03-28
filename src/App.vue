65f8126bfe220deab2238d638b153016da20faba
lifei-2019 <940915122@qq.com>
2022-03-28 15:48:21 +0800

    columnlist简单搭建

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介， 尽情期待 ',
+    avatar: 'https://cn.vuejs.org/images/logo.png'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '这是test2的专栏，有一段非常有意思的简介， 尽情期待 ',
+    // avatar: 'http://vue-maker.oss-cn-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    avatar: 'https://cn.vuejs.org/images/logo.png'
+  }
+]
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+      <img :src="column.avatar" :alt="column.title" height="30" width="30">
+      <h5>{{column.title}}</h5>
+      <p>{{column.description}}</p>
+      <a href="#">进入专栏</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, PropType } from 'vue'
+
+export interface ColumnProps {
+  id:number;
+  title:string;
+  avatar:string;
+  description:string;
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      // 把构造函数断言成类型 需要借用PropType，这样整个模版中都能实现
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  setup (props) {
+    // props.list[0].id
+  }
+})
+</script>
